9e5b3d3ca0a64dca2d1c2f22f771ccca3089a4f0
rawziNael <rawzi.homran@yahoo.com>
2022-03-09 18:03:35 +0300

    class07

--- linked-list/app/src/main/java/linked/list/LinkedList.java
+++ linked-list/app/src/main/java/linked/list/LinkedList.java
@@ -1,13 +1,12 @@
 package linked.list;
 
 public class LinkedList {
-
         public Node head;
 
         public LinkedList() {
 
         };
-
+   // Code Challenge: Class 05: Linked List Implementation
    public void insert(int val) {
             Node newNode = new Node(val);
             newNode.next = head;
@@ -39,6 +38,8 @@ public class LinkedList {
             return str;
     }
 
+    //Code Challenge: Class 06 - Extend a Linked List to allow various insertion methods.
+
     public void append(int val) {
         Node newNode = new Node(val);
         Node runner = head;
@@ -66,5 +67,26 @@ public class LinkedList {
         }
         newNode.next = runner.next;
         runner.next = newNode;
+    }
+
+    //Code Challenge: Class 07 - (k-th value from the end of a linked list).
+
+    public int getKValue(int k) {
+        Node current = this.head;
+
+        while (current != null) {
+            Node kFromEnd = current;
+            for (int i = 0; i < k; i++) {
+                if (kFromEnd.next == null && i != k - 1) {
+                    return -1;
+                }
+                kFromEnd = kFromEnd.next;
+            }
+            if (kFromEnd.next == null) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        return -1;
     }
 }
