fd0784184d215258634173eee7129329e6b1f6e5
rawziNael <rawzi.homran@yahoo.com>
2022-04-07 11:56:20 +0300

    class18

--- tree/lib/src/main/java/tree/MaximumValue.java
+++ tree/lib/src/main/java/tree/MaximumValue.java
@@ -1,43 +1,20 @@
 package tree;
 
-<<<<<<< HEAD
-public class MaximumValue{
-        static Node root;
+public class MaximumValue {
+    static Node root;
 
-        static int findMax(Node node) {
-            if (node == null)
-                return Integer.MIN_VALUE;
+    static int findMax(Node node) {
+        if (node == null)
+            return Integer.MIN_VALUE;
 
-            int result = (int) node.value;
-            int left = findMax(node.left);
-            int right = findMax(node.right);
+        int result = (int) node.value;
+        int left = findMax(node.left);
+        int right = findMax(node.right);
 
-            if (left > result)
-                result = left;
-            if (right > result)
-                result = right;
-            return result;
-        }
-=======
-public class MaximumValue extends BinaryTree{
-    static Integer maxValue;
-
-    public MaximumValue() {
+        if (left > result)
+            result = left;
+        if (right > result)
+            result = right;
+        return result;
     }
-
-    public static Integer findMaximumValue(BinaryTree tree) {
-        if (tree.root == null) {
-            return null;
-        }
-        MaximumValue.maxValue = (Integer) tree.root.value;
-        MaximumValue.recurSearch(tree.root);
-        return maxValue;
-    }
-
-    public static void recurSearch(Node node) {
-        if ((Integer) node.value > MaximumValue.maxValue) maxValue = (Integer) node.value;
-        if (node.left != null) recurSearch(node.left);
-        if (node.right != null) recurSearch(node.right);
-    }
->>>>>>> main
 }
